967c6bb59a78597c81508f6c7e3771f621a92a40
harneeetah <harneeetah@gmail.com>
2024-03-19 16:41:21 +0100

    register

--- config/config.php
+++ config/config.php
@@ -10,11 +10,11 @@ define("DBNAME","coffee-blend");
 define("USER", "root");
 
 //password
-define("PASS", "");
+define("PASS", "annie");
 
 $conn = new PDO("mysql:host=" .HOST.";dbname=".DBNAME."", USER, PASS);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-} catch( PDOException $Exception ) {
-   echo  $Exception->getMessage( );
+} catch(PDOException $Exception) {
+   echo  $Exception->getMessage();
 }
